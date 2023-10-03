8b93490a3d0fcd3a9aa9155bb7228e839a35844e
thinkySide <eunlyuing@gmail.com>
2023-10-03 15:29:41 +0900

    Add struct ThemeColor

---
+++ TIP-Calculator/TIP-Calculator/Config/ThemeColor.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColor.swift
+//  TIP-Calculator
+//
+//  Created by 김민준 on 10/3/23.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let bg = UIColor(hexCode: "F5F3F4")
+    static let primary = UIColor(hexCode: "1CC9BE")
+    static let secondary = UIColor.systemOrange
+    static let text = UIColor(hexCode: "000000")
+    static let separator = UIColor(hexCode: "CCCCCC")
+}
---
+++ TIP-Calculator/TIP-Calculator/Config/ThemeFont.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeFont.swift
+//  TIP-Calculator
+//
+//  Created by 김민준 on 10/3/23.
+//
+
+import Foundation
