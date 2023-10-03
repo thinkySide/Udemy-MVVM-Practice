6aeac4fdebaeea0533fc73a5840f2c07bedc1802
thinkySide <eunlyuing@gmail.com>
2023-10-03 12:46:28 +0900

    Create iOS new project

---
+++ TIP-Calculator/TIP-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIP-Calculator
+//
+//  Created by 김민준 on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
