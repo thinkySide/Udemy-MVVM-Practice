887432e33138619714851f39a82a1d7cfe24f498
thinkySide <eunlyuing@gmail.com>
2023-10-03 15:23:10 +0900

    Add CalculatorVC vStackView

--- TIP-Calculator/TIP-Calculator/CalculatorVC.swift
+++ TIP-Calculator/TIP-Calculator/CalculatorVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class CalculatorVC: UIViewController {
     
@@ -14,12 +15,57 @@ class CalculatorVC: UIViewController {
     private let billInputView = BillInputView()
     private let tipInputView = TipInputView()
     private let splitInputView = SplitInpitView()
+    
+    private lazy var vStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            logoView,
+            resultView,
+            billInputView,
+            tipInputView,
+            splitInputView
+        ])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 36
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        layout()
     }
 
-
+    private func layout() {
+        
+        view.backgroundColor = .white
+        view.addSubview(vStackView)
+        
+        vStackView.snp.makeConstraints { make in
+            make.leading.equalTo(view.snp.leadingMargin).offset(16)
+            make.trailing.equalTo(view.snp.trailingMargin).offset(-16)
+            make.bottom.equalTo(view.snp.bottomMargin).offset(-16)
+            make.top.equalTo(view.snp.topMargin).offset(16)
+        }
+        
+        logoView.snp.makeConstraints { make in
+            make.height.equalTo(48)
+        }
+        
+        resultView.snp.makeConstraints { make in
+            make.height.equalTo(224)
+        }
+        
+        billInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+        
+        tipInputView.snp.makeConstraints { make in
+            make.height.equalTo(56+56+16)
+        }
+        
+        splitInputView.snp.makeConstraints { make in
+            make.height.equalTo(56)
+        }
+    }
 }
 
