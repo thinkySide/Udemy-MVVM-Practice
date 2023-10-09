19cc2dcc0e6da4a0829f64ce9db1b4a24a2537a2
thinkySide <eunlyuing@gmail.com>
2023-10-09 17:38:19 +0900

    Complete Snapshot test

--- TIP-Calculator/TIP-CalculatorTests/Tip_CalculatorSnapshotTest.swift
+++ TIP-Calculator/TIP-CalculatorTests/Tip_CalculatorSnapshotTest.swift
@@ -29,9 +29,25 @@ final class Tip_CalculatorSnapshotTest: XCTestCase {
         assertSnapshot(matching: view, as: .image(size: size))
     }
     
+    func testResultViewWithValue() {
+        let size = CGSize(width: screenWidth, height: 224)
+        let result = Result(amountPerPerson: 100.25, totalBill: 45, totalTip: 60)
+        let view = ResultView()
+        view.configure(result: result)
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
     func testInitialBillInputView() {
         let size = CGSize(width: screenWidth, height: 56)
         let view = BillInputView()
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testBillInputViewWithValue() {
+        let size = CGSize(width: screenWidth, height: 56)
+        let view = BillInputView()
+        let textField = view.allSubViewsOf(type: UITextField.self).first
+        textField?.text = "500"
         assertSnapshot(matching: view, as: .image(size: size))
     }
     
@@ -41,9 +57,40 @@ final class Tip_CalculatorSnapshotTest: XCTestCase {
         assertSnapshot(matching: view, as: .image(size: size))
     }
     
+    func testTipInputViewWithSelection() {
+        let size = CGSize(width: screenWidth, height: 56+56+16)
+        let view = TipInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).first
+        button?.sendActions(for: .touchUpInside)
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
     func testInitialSplitInputView() {
         let size = CGSize(width: screenWidth, height: 56)
         let view = SplitInputView()
         assertSnapshot(matching: view, as: .image(size: size))
+    }
+    
+    func testSplitInputViewWithSelection() {
+        let size = CGSize(width: screenWidth, height: 56)
+        let view = SplitInputView()
+        let button = view.allSubViewsOf(type: UIButton.self).last
+        button?.sendActions(for: .touchUpInside)
+        assertSnapshot(matching: view, as: .image(size: size))
+    }
+}
+
+extension UIView {
+    func allSubViewsOf<T: UIView>(type: T.Type) -> [T] {
+        var all = [T]()
+        func getSubView(view: UIView) {
+            if let aView = view as? T {
+                all.append(aView)
+            }
+            guard view.subviews.count > 0 else { return }
+            view.subviews.forEach { getSubView(view: $0) }
+        }
+        getSubView(view: self)
+        return all
     }
 }
